3ed0fd3c89677a7863b72281e9c9cb4a466d5fae
Hayk <hayk.hayr3@gmail.com>
2020-06-25 04:08:49 +0400

    Add project files.

---
+++ MetanitLesson1.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MetanitLesson1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num == 5 || num == 10)
+            {
+                Console.WriteLine("Your integer is either 5 or 10!");
+            }
+            else
+            {
+                Console.WriteLine("Unknown integer");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
